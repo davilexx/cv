73442ef518878fc7304fb512e83c94f7ae949257
Alexander Kharmansky <davilex_x@vk.com>
2023-05-17 16:18:32 +0600

    refactor: add basic file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Project } from './components';
+import {
+  Header,
+  Home,
+  Skills,
+  Projects,
+  Experience,
+  Education,
+  Footer,
+} from './containers';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Home />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      <Footer />
     </div>
   );
 }
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from './header/Header';
+export { default as Home } from './home/Home';
+export { default as Skills } from './skills/Skills';
+export { default as Projects } from './projects/Projects';
+export { default as Experience } from './experience/Experience';
+export { default as Education } from './education/Education';
+export { default as Footer } from './footer/Footer';
